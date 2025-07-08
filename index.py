eef0375e602ce23c6658a3296d2dc93df959fb62
chrigesch <christianschetsche@psi.uba.ar>
2025-07-08 10:57:39 -0300

    feat: added support for "Recuperatorio"

--- index.py
+++ index.py
@@ -16,6 +16,13 @@ OPERACIONES_DISPONIBLES = (
     "Parcial 2",
     "Condiciones preliminares",
     "Diferencias entre dos actas",
+    "Recuperatorio",
+)
+
+COND_PROM_DISPONIBLES = (
+    "cond_prom_6_y_6",
+    "cond_prom_6_y_7",
+    "cond_prom_7_y_7",
 )
 
 
@@ -31,6 +38,13 @@ def main():
         label="**Seleccionar el listado requerido**",
         options=OPERACIONES_DISPONIBLES,
     )
+    if operacion == "Recuperatorio":
+        cond_promocion = st.selectbox(
+            label="**Seleccionar la condición para promocionar**",
+            options=COND_PROM_DISPONIBLES,
+            index=1,
+        )
+
     # Sacar listado para habilitados
     if operacion in ("Parcial 1", "Parcial 2"):
         col_1, col_2 = st.columns(2)
@@ -229,6 +243,185 @@ def main():
             )
             st.warning(body=texto)
             st.dataframe(listado_diferencias)
+    # Sacar listados para el recuperatorio
+    if operacion == "Recuperatorio":
+        col_1, col_2, col_3, col_4 = st.columns(4)
+        with col_1:
+            # Create file uploader object
+            uploaded_file_actas = st.file_uploader(
+                label="**Cargar el Excel de 'actas'**",
+                type=["xlsx"],
+            )
+            if uploaded_file_actas is not None:
+                listado_actas = pd.read_excel(io=uploaded_file_actas)
+        with col_2:
+            # Create file uploader object
+            uploaded_file_campus_notas = st.file_uploader(
+                label="**Cargar el Excel del campus que incluye las notas de los DOS parciales**",
+                type=["xlsx"],
+            )
+            if uploaded_file_campus_notas is not None:
+                listado_campus_notas = pd.read_excel(io=uploaded_file_campus_notas)
+                cols = "\n \n".join(listado_campus_notas.columns)
+                st.warning(
+                    f"Revisar orden de columnas del listado y corregir, si necesario:\n \n{cols}"
+                )
+        with col_3:
+            # Create file uploader object
+            uploaded_file_campus_autoevaluaciones = st.file_uploader(
+                label="**Cargar el Excel del campus que incluye las OCHO autoevaluaciones**",
+                type=["xlsx"],
+            )
+            if uploaded_file_campus_autoevaluaciones is not None:
+                listado_campus_autoevaluaciones = pd.read_excel(
+                    io=uploaded_file_campus_autoevaluaciones
+                )
+                cols = "\n \n".join(listado_campus_autoevaluaciones.columns)
+                st.warning(
+                    f"Revisar orden de columnas del listado y corregir, si necesario:\n \n{cols}"
+                )
+        with col_4:
+            # Create file uploader object
+            uploaded_file_certificados = st.file_uploader(
+                label="**Cargar el Excel con los certificados**",
+                type=["xlsx"],
+            )
+            if uploaded_file_certificados is not None:
+                listado_certificados = pd.read_excel(io=uploaded_file_certificados)
+            else:
+                listado_certificados = None
+
+        if (
+            (uploaded_file_actas is None)
+            | (uploaded_file_campus_notas is None)
+            | (uploaded_file_campus_autoevaluaciones is None)
+            | (uploaded_file_certificados is None)
+        ):
+            st.stop()
+
+        resultado_actas_notas = cruzar_listas_actas_notas(
+            listado_actas=listado_actas,
+            listado_campus=listado_campus_notas,
+            listado_certificados=listado_certificados,
+            cond_promocion=cond_promocion,
+            condicion="preliminar",
+            crear_excel=False,
+            mostrar_alumnos_no_encontrados=False,
+            mostrar_alumnos_corregidos=False,
+            mostrar_duplicados_campus=False,
+        )
+        listado_actas_recupera_1 = listado_actas[
+            resultado_actas_notas["listas"]["todos"]["recuperatorio"] == 1
+        ]
+        listado_actas_recupera_2 = listado_actas[
+            resultado_actas_notas["listas"]["todos"]["recuperatorio"] == 2
+        ]
+        resultado_actas_autoevaluaciones_1 = cruzar_listas_actas_autoevaluaciones(
+            listado_actas=listado_actas_recupera_1,
+            listado_campus=listado_campus_autoevaluaciones,
+            parcial="recuperatorio",
+            crear_excel=False,
+            mostrar_alumnos_no_encontrados=False,
+            mostrar_alumnos_corregidos=False,
+            mostrar_duplicados_campus=False,
+        )
+        resultado_actas_autoevaluaciones_2 = cruzar_listas_actas_autoevaluaciones(
+            listado_actas=listado_actas_recupera_2,
+            listado_campus=listado_campus_autoevaluaciones,
+            parcial="recuperatorio",
+            crear_excel=False,
+            mostrar_alumnos_no_encontrados=False,
+            mostrar_alumnos_corregidos=False,
+            mostrar_duplicados_campus=False,
+        )
+
+        len_en_actas_pero_no_en_campus = len(
+            resultado_actas_notas["correcciones"]["en_actas_pero_no_en_campus"]
+        )
+        if len_en_actas_pero_no_en_campus == 0:
+            texto = "Todos los alumnos del acta se encontraron en el listado campus"
+            st.success(body=texto)
+        else:
+            texto = f"Cuidado: {len_en_actas_pero_no_en_campus} alumnos en actas, pero NO en el campus:"
+            st.warning(body=texto)
+
+        # Pestañas para todos los resultados
+        tab_1, tab_2, tab_3, tab_4 = st.tabs(
+            tabs=[
+                "**Listas condiciones preliminares**",
+                "**Listas parcial 1**",
+                "**Listas parcial 2**",
+                "**Correcciones**",
+            ]
+        )
+        with tab_1:
+            listados_disponibles = resultado_actas_notas["listas"].keys()
+            listado_seleccionado = st.selectbox(
+                label="**Seleccionar el listado requerido**",
+                options=listados_disponibles,
+            )
+            st.dataframe(resultado_actas_notas["listas"][listado_seleccionado])
+        with tab_2:
+            listados_disponibles = resultado_actas_autoevaluaciones_1["listas"].keys()
+            listado_seleccionado = st.selectbox(
+                label="**Seleccionar el listado requerido**",
+                options=listados_disponibles,
+            )
+            st.dataframe(
+                resultado_actas_autoevaluaciones_1["listas"][listado_seleccionado]
+            )
+        with tab_3:
+            listados_disponibles = resultado_actas_autoevaluaciones_2["listas"].keys()
+            listado_seleccionado = st.selectbox(
+                label="**Seleccionar el listado requerido**",
+                options=listados_disponibles,
+            )
+            st.dataframe(
+                resultado_actas_autoevaluaciones_2["listas"][listado_seleccionado]
+            )
+        with tab_4:
+            listados_disponibles = resultado_actas_notas["correcciones"].keys()
+            listado_seleccionado = st.selectbox(
+                label="**Seleccionar el listado requerido**",
+                options=listados_disponibles,
+            )
+            st.dataframe(resultado_actas_notas["correcciones"][listado_seleccionado])
+
+        col_1, col_2, col_3 = st.columns(3)
+        with col_1:
+            excel_bytes = _crear_excel_descargable(resultado_actas_notas["listas"])
+            now = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+            nombre_archivo = f"listado_condiciones_preliminares_{now}.xlsx"
+
+            st.download_button(
+                label="Descargar el listado de condiciones preliminares",
+                data=excel_bytes,
+                file_name=nombre_archivo,
+            )
+        with col_2:
+            excel_bytes = _crear_excel_descargable(
+                resultado_actas_autoevaluaciones_1["listas"]
+            )
+            now = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+            nombre_archivo = f"listado_habilitados_parcial_1_{now}.xlsx"
+
+            st.download_button(
+                label="Descargar el listado de habilitadados para rendir el parcial 1",
+                data=excel_bytes,
+                file_name=nombre_archivo,
+            )
+        with col_3:
+            excel_bytes = _crear_excel_descargable(
+                resultado_actas_autoevaluaciones_2["listas"]
+            )
+            now = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+            nombre_archivo = f"listado_habilitados_parcial_2_{now}.xlsx"
+
+            st.download_button(
+                label="Descargar el listado de habilitadados para rendir el parcial 2",
+                data=excel_bytes,
+                file_name=nombre_archivo,
+            )
 
 
 if __name__ == "__main__":
