f1297358c6887973ccb7b61ea3c97b3077215287
chrigesch <christianschetsche@psi.uba.ar>
2025-07-08 11:05:21 -0300

    feat: added support for "Condiciones finales"

--- index.py
+++ index.py
@@ -17,6 +17,7 @@ OPERACIONES_DISPONIBLES = (
     "Condiciones preliminares",
     "Diferencias entre dos actas",
     "Recuperatorio",
+    "Condiciones finales",
 )
 
 COND_PROM_DISPONIBLES = (
@@ -38,7 +39,7 @@ def main():
         label="**Seleccionar el listado requerido**",
         options=OPERACIONES_DISPONIBLES,
     )
-    if operacion == "Recuperatorio":
+    if operacion in ["Recuperatorio", "Condiciones finales"]:
         cond_promocion = st.selectbox(
             label="**Seleccionar la condición para promocionar**",
             options=COND_PROM_DISPONIBLES,
@@ -422,6 +423,94 @@ def main():
                 data=excel_bytes,
                 file_name=nombre_archivo,
             )
+    # Sacar listado con condiciones finales
+    if operacion == "Condiciones finales":
+        col_1, col_2, col_3 = st.columns(3)
+        with col_1:
+            # Create file uploader object
+            uploaded_file_actas = st.file_uploader(
+                label="**Cargar el Excel de 'actas'**",
+                type=["xlsx"],
+            )
+            if uploaded_file_actas is not None:
+                listado_actas = pd.read_excel(io=uploaded_file_actas)
+        with col_2:
+            # Create file uploader object
+            uploaded_file_campus = st.file_uploader(
+                label="**Cargar el Excel del campus que incluye las notas de los DOS parciales y del RECUPERATORIO**",
+                type=["xlsx"],
+            )
+            if uploaded_file_campus is not None:
+                listado_campus = pd.read_excel(io=uploaded_file_campus)
+                cols = "\n \n".join(listado_campus.columns)
+                st.warning(
+                    f"Revisar orden de columnas del listado y corregir, si necesario:\n \n{cols}"
+                )
+        with col_3:
+            # Create file uploader object
+            uploaded_file_certificados = st.file_uploader(
+                label="**Cargar el Excel con los certificados**",
+                type=["xlsx"],
+            )
+            if uploaded_file_certificados is not None:
+                listado_certificados = pd.read_excel(io=uploaded_file_certificados)
+            else:
+                listado_certificados = None
+
+        if (
+            (uploaded_file_actas is None)
+            | (uploaded_file_campus is None)
+            | (uploaded_file_certificados is None)
+        ):
+            st.stop()
+
+        resultado_actas_notas = cruzar_listas_actas_notas(
+            listado_actas=listado_actas,
+            listado_campus=listado_campus,
+            listado_certificados=listado_certificados,
+            cond_promocion=cond_promocion,
+            condicion="final",
+            crear_excel=False,
+            mostrar_alumnos_no_encontrados=False,
+            mostrar_alumnos_corregidos=False,
+            mostrar_duplicados_campus=False,
+        )
+        len_en_actas_pero_no_en_campus = len(
+            resultado_actas_notas["correcciones"]["en_actas_pero_no_en_campus"]
+        )
+        if len_en_actas_pero_no_en_campus == 0:
+            texto = "Todos los alumnos del acta se encontraron en el listado campus"
+            st.success(body=texto)
+        else:
+            texto = f"Cuidado: {len_en_actas_pero_no_en_campus} alumnos en actas, pero NO en el campus:"
+            st.warning(body=texto)
+
+        # Pestañas para todos los resultados
+        tab_1, tab_2 = st.tabs(tabs=["**Listas**", "**Correcciones**"])
+        with tab_1:
+            listados_disponibles = resultado_actas_notas["listas"].keys()
+            listado_seleccionado = st.selectbox(
+                label="**Seleccionar el listado requerido**",
+                options=listados_disponibles,
+            )
+            st.dataframe(resultado_actas_notas["listas"][listado_seleccionado])
+        with tab_2:
+            listados_disponibles = resultado_actas_notas["correcciones"].keys()
+            listado_seleccionado = st.selectbox(
+                label="**Seleccionar el listado requerido**",
+                options=listados_disponibles,
+            )
+            st.dataframe(resultado_actas_notas["correcciones"][listado_seleccionado])
+
+        excel_bytes = _crear_excel_descargable(resultado_actas_notas["listas"])
+        now = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+        nombre_archivo = f"listado_condiciones_finales_{now}.xlsx"
+
+        st.download_button(
+            label="Descargar el listado de condiciones finales",
+            data=excel_bytes,
+            file_name=nombre_archivo,
+        )
 
 
 if __name__ == "__main__":
